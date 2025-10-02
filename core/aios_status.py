ed0dd5e63772256bc75da0b08566d33ca428489f
UbuntuSSD <spatten2@fordham.edu>
2025-10-02 16:50:54 -0400

    consolidate start, remove walrus, increase start speed

--- core/aios_status.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import sys
-sys.path.append('/home/seanpatten/projects/AIOS')
-from core import aios_db
-print(f"PIDs: {aios_db.read('aios_pids')}")
--- core/aios_stop.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import subprocess, sys
-sys.path.append('/home/seanpatten/projects/AIOS')
-from core import aios_db
-subprocess.run(["pkill", "-9", "-f", "core/aios_api.py"], stderr=subprocess.DEVNULL)
-subprocess.run(["pkill", "-9", "-f", "services/web/web.py"], stderr=subprocess.DEVNULL)
-aios_db.write("aios_pids", {})
-print("AIOS stopped")
